fc0b270201fd172aef8598a5c8cf861fe4d23d72
Humizi <Humizi307@gmail.com>
2020-08-19 17:18:23 +1000

    login

--- src/app/modules/main-page/main.page.ts
+++ src/app/modules/main-page/main.page.ts
@@ -6,12 +6,23 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MainPage {
   public data: any;
+  public auth = {
+    login: "tech",
+    password: "tech_password"
+  }
 
   constructor(
     private http: HttpClient
   ) {
-    this.http.get('').subscribe(data => {
-      this.data = data;
+    this.http.post('http://192.168.88.139:1020/auth/login', this.auth).subscribe((user: any) => {
+      console.log(user.data.access_token)
+      this.http.get(`http://192.168.88.139:1020/v1/crm/requests/1`, {
+        headers: {
+          Authorization: `Bearer ${user.data.access_token}`
+        }
+      }).subscribe(data => {
+        console.log(data)
+      })
     })
   }
 }
